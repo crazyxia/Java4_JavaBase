ffbf2977085d17f900a618b4e06a91c43ac3175e
xiachaochao <2252916592@qq.com>
2017-03-30 19:18:23 +0800

    try catch

---
+++ JavaBase/src/com/xinan/exception/Exception01.java
@@ -0,0 +1,27 @@
+package com.xinan.exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exception01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("大爷，请输入两个数");
+		try {
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			System.out.println("您输入的两个数的商为:" + num1 / num2);
+		} catch (ArithmeticException ae) {
+			ae.printStackTrace();
+			System.out.println("ArithmeticException被捕获");
+		} catch (InputMismatchException ime) {
+			ime.printStackTrace();
+			//System.out.println(ime.getMessage());
+			//System.out.println("InputMismatchException被捕获");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			System.out.println("Exception被捕获");
+		}
+	}
+}
---
+++ JavaBase/src/com/xinan/exception/Lpg4jDemo.java
@@ -0,0 +1,13 @@
+package com.xinan.exception;
+
+import org.apache.log4j.Logger;
+
+public class Lpg4jDemo {
+	private Logger logger = Logger.getLogger(this.getClass());
+	public  void logDemo(){
+		logger.debug("这是debug级别的信息");
+	}
+	public static void main(String[] args) {
+
+	}
+}
