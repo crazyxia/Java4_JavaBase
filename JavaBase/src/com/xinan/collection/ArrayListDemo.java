0086590ba745e2b599edba7c20cdf11ea2e0530d
xiachaochao <2252916592@qq.com>
2017-03-31 19:30:55 +0800

    12

--- JavaBase/src/com/xinan/collection/ArrayListDemo.java
+++ JavaBase/src/com/xinan/collection/ArrayListDemo.java
@@ -7,6 +7,7 @@ package com.xinan.collection;
  *
  */
 import java.util.ArrayList;
+import java.util.Iterator;
 public class ArrayListDemo {
 	public static void main(String[] args){
 	ArrayList<String> list1=new ArrayList<String>();
@@ -16,11 +17,19 @@ public class ArrayListDemo {
 	list1.add("L");
 	list1.add("O");
 	System.out.println(list1);//打印输出
-	for(String string: list1){//使用增强for循环，遍历元素
+	Iterator<String> iter=list1.iterator();//使用iterator进行遍历
+	while(iter.hasNext()){
+		System.out.println(iter.next());
+	}
+	System.out.println("这是通过iterator进行的循环");
+	/*for(String string: list1){//使用增强for循环，遍历元素
 		System.out.println(string);
 	}
 	list1.add(0, "Ilove");//在特定位置增加元素
 		System.out.println("这是增加完Ilove之后打印的"+list1);
-	
+	*/
+	list1.remove(1);//通过索引删除
+	System.out.println("remove后的集合"+list1);
+	System.out.println(list1.size());
 	}
 }
