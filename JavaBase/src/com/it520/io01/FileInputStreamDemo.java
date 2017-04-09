28607a2ad0dd85c38d2d9bae069a04fb0adca659
crazy <2252916592@qq.com>
2017-04-09 19:36:04 +0800

    2017.04.09 19：36

--- JavaBase/src/com/it520/io01/FileInputStreamDemo.java
+++ JavaBase/src/com/it520/io01/FileInputStreamDemo.java
@@ -11,21 +11,26 @@ import java.io.OutputStream;
 public class FileInputStreamDemo {
 	public static void main(String[] args) throws IOException{
 		//定义一个file对象
-		File f1=new File("poweroff.txt");
-		File f2=new File("copy.txt");
+		final int BUFFER_SIZE=1024;//定义数组常量，便于修改
+		File f1=new File("123.avi");
+		File f2=new File("copy.avi");
 		System.out.println(f1.getAbsolutePath());
 		//定义流对象
 		InputStream fis=new FileInputStream(f1);
 		OutputStream fos=new FileOutputStream(f2);
 		//流操作
-		byte[] b1=new byte[5];//数组中需要定义长度
+		byte[] b1=new byte[BUFFER_SIZE];//数组中需要定义长度
 		int len=0;
-		//len=fis.read(b1);
+		len=fis.read(b1);
 		while(len!=-1){
-			len=fis.read(b1);
 			//String str=new String(b1,0,len);
 			//System.out.println(str);
 			fos.write(b1, 0, len);		
+			len=fis.read(b1);
+			/*fos.write(b1, 0, len);		
+			len=fis.read(b1);
+			注意这两句话的顺序，写反了容易出现问题，
+			因为前面len=fis.read(b1)已经做了一次读操作了*/
 		}
 		System.out.println();
 		//关闭流操作
