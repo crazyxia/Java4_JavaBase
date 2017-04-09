127b72d1507c11781920bb2c38e59b7fdb5134db
crazy <2252916592@qq.com>
2017-04-09 17:37:39 +0800

    1

--- JavaBase/src/com/it520/io01/FileInputStreamDemo.java
+++ JavaBase/src/com/it520/io01/FileInputStreamDemo.java
@@ -2,25 +2,34 @@ package com.it520.io01;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class FileInputStreamDemo {
-	public static void main(String[] args) throws Exception{
-		//1.创建目标对象（表示数据保存到哪一个文件）
-		File f1=new File("file/stream.txt");
-		
-		//2.创建水管
-		InputStream input=new FileInputStream(f1);//InputStream和OutputStream不能直接new
-		//3.具体操作
-		byte[] b1=new byte[5];//
-		int len=-1;
-		len=input.read(b1);
+	public static void main(String[] args) throws IOException{
+		//定义一个file对象
+		File f1=new File("poweroff.txt");
+		File f2=new File("copy.txt");
+		System.out.println(f1.getAbsolutePath());
+		//定义流对象
+		InputStream fis=new FileInputStream(f1);
+		OutputStream fos=new FileOutputStream(f2);
+		//流操作
+		byte[] b1=new byte[5];//数组中需要定义长度
+		int len=0;
+		//len=fis.read(b1);
 		while(len!=-1){
-			String str=new String(b1,0,len);
-			System.out.println(str);
+			len=fis.read(b1);
+			//String str=new String(b1,0,len);
+			//System.out.println(str);
+			fos.write(b1, 0, len);		
 		}
-		//4.关闭操作
-		input.close();
+		System.out.println();
+		//关闭流操作
+		fis.close();
+		fos.close();
 	}
-	
 }
